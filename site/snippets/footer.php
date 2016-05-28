baef98977da0b48b1a52471192876351d639ed03
Nathan Chapman <nathan@nchp.mn>
2016-05-28 15:56:27 +1000

    Add design to header.php and footer.php

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,2 +1,20 @@
+<div class="footer">
+                <div class="pure-menu pure-menu-horizontal">
+                    <ul>
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Contact Us</a></li>
+                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">About</a></li>
+                        <li class="pure-menu-item"><a href="http://relativity.com.au" class="pure-menu-link">Relativity Studios</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
 </body>
 </html>
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,11 +1,54 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Podcast Time, an independant production created at Relativity Studios">
 
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Podcast Time</title>
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+    
+
+    <!-- Pure CSS Framework -->
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+  
+    <!-- Custom CSS -->
+    <?php echo css('assets/css/main.min.css') ?>
+  
+
+
+    
+
+    
 
 </head>
 <body>
+
+
+
+
+
+
+
+<div id="layout" class="pure-g">
+    <!-- The sidebar and nav area -->
+    <div class="sidebar pure-u-1 pure-u-md-1-4">
+        <div class="header">
+            <h1 class="brand-title">NKB Podcast</h1>
+            <h2 class="brand-tagline">A podcast during the work day.</h2>
+
+            <nav class="nav">
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a class="pure-button" href="#">Episodes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="pure-button" href="#">About</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    
+        <div class="content pure-u-1 pure-u-md-3-4">
