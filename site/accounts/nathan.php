3d10f15d98dbcf03783f2717e671da6ed51aca68
Nathan Chapman <nathan@nchp.mn>
2016-05-28 15:51:49 +1000

    Start setting up Blueprints

---
+++ site/accounts/nathan.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: nathan
+password: >
+  $2a$10$N9n8v7FsCuQXdlQ0FEDvB.BRT9Mv/oqG8pQGL8tkvi3SfL7LEnHj6
+email: nathan@nchp.mn
+language: en
+role: admin
+history:
+  - episodes
