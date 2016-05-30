b688ebfdfa15a9c15ed080d23f530366ca201961
Nathan Chapman <nathan@nchp.mn>
2016-05-30 21:53:06 +1000

    Add basic snippets and templates for `blog` and `episode`

---
+++ site/snippets/single-blog.php
@@ -0,0 +1,17 @@
+<section class="post blog">
+    <header class="post-header">
+        <img class="post-avatar" alt="Eric Ferraiuolo&#x27;s avatar" height="48" width="48" src="img/common/ericf-avatar.png">
+
+        <h2 class="post-title"><?php echo $blog->title() ?></h2>
+
+        <p class="post-meta">
+            By <a class="post-author" href="#">Kira</a> under <a class="post-category post-category-blog" href="#">Blog</a>
+        </p>
+    </header>
+
+    <div class="post-description">
+        <p>
+            Oh how meta is this, declaring you've a new website on the dev page of the new site! You hipster wanker. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+    </div>
+</section>
--- site/templates/home.php
+++ site/templates/home.php
@@ -6,4 +6,9 @@ First, the latest podcast episode
 
 Now all the episodes
 
+<?php foreach(page('posts')->children()->visible()->flip()->limit(4) as $post): ?>
+<?php snippet('single-blog', array('blog' => $post)) ?>
+
+<?php endforeach ?>
+
 <?php snippet('footer') ?>
