0c7e7281af3974b98fbc3e6d71e382e96dd829b8
Nathan Chapman <nathan@nchp.mn>
2016-05-30 22:10:45 +1000

    Enable Markdown Extra

--- site/config/config.php
+++ site/config/config.php
@@ -15,7 +15,7 @@ for more information: http://getkirby.com/license
 
 */
 
-c::set('license', 'put your license key here');
+c::set('license', 'itspodcasttime');
 
 /*
 
@@ -28,3 +28,7 @@ make Kirby work. For more fine-grained configuration
 of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
+
+
+/* Enable Markdown Extra Features */
+c::set('markdown.extra',false);
