0a05b78655cea82ab8e256468e929240044e8f7c
Nathan Chapman <nathan@nchp.mn>
2016-06-22 17:59:28 +1000

    Add gorgeous sidebar background!

--- site/snippets/single-episode.php
+++ site/snippets/single-episode.php
@@ -11,8 +11,21 @@
         </p>
     </header>
     
-    <?php echo $blog->embed()->oembed() ?>
+<div class="container">
+  <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F270095690"></iframe>
+  <p>Setting soundcloud player's width to 100% makes it cheese.</p>
+</div>
+    
+    .container {
+  width: 90%;
+  max-width:50em;
+  margin: 0 auto;
+  height: 4em;
+}
 
+.container iframe { max-height:6em; }
+    
+    
     <div class="post-description">
         <p><?php echo str::excerpt($blog->text()->kirbytext(), 500, FALSE);
  ?></p>
